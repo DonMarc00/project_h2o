4f9a69116bf4d22fe0f2b92b309f14ad90c347b1
Marc Novak <marc.novak1@web.de>
2023-11-29 20:35:21 +0100

    Added file for db_service_tests

--- test/services/db_service_test.dart
+++ test/services/db_service_test.dart
@@ -1,12 +1,20 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/mockito.dart';
+import 'package:sqflite/sqflite.dart';
 import 'package:project_h2o/services/db_service.dart';
 import 'package:project_h2o/db_models/reminder_model.dart';
+import 'package:project_h2o/utils/date_utils.dart';
 
-import '../mocks/mock_db.dart';
+class MockDatabase extends Mock implements Database {}
 
 void main() {
   group('DBService Tests', () {
-    // Your tests go here
+    late MockDatabase mockDatabase;
+    late DBService dbService;
+
+    setUp(() {
+      mockDatabase = MockDatabase();
+      dbService = DBService(mockDatabase);
+    });
   });
 }
